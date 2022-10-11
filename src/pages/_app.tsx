f6068eea2b2d5032aeed8486701c7cbae472d00d
IdrisIsra <israpilov.idris@gmail.com>
2022-10-11 21:58:22 +0300

    moving next/head out of pages into _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,23 @@ import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const title = "Ephemeral Question";
+  const description = "Ask some questions, give some answers.";
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
